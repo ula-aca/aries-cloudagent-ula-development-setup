398876e92f9f908d025558a849b0fcd50a16ea5c
Timo Glastra <timo@animo.id>
2021-02-03 12:16:21 +0100

    add demo
    Signed-off-by: Timo Glastra <timo@animo.id>

---
+++ demo/precache-manifest.3c759479bf12fd5eafb4a5e703651707.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08478e97981342c541549ba96cb9f0d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cb5a25b8a6c917d14a8",
+    "url": "/static/js/2.1688f0b3.chunk.js"
+  },
+  {
+    "revision": "7163fde506b619d94e8ad5d51aed82fb",
+    "url": "/static/js/2.1688f0b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "32422273b4292d5d8e63",
+    "url": "/static/js/main.e9a65684.chunk.js"
+  },
+  {
+    "revision": "ad6e2c9d01feb0e028d0",
+    "url": "/static/js/runtime-main.715f6818.js"
+  }
+]);
